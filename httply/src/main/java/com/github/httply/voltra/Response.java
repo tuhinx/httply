f3b863adddda7afa759b7107d7e76166fefcec4a
softdev-pro <info.nbsoftdev@gmail.com>
2025-05-10 02:26:36 +0600

    Init Update

---
+++ httply/src/main/java/com/github/httply/voltra/Response.java
@@ -0,0 +1,26 @@
+package com.github.httply.voltra;
+
+/**
+ * Response listener interface for compatibility with example usage pattern.
+ */
+public class Response {
+    /**
+     * Listener interface for receiving the response.
+     */
+    public interface Listener<T> {
+        /**
+         * Called when a response is received.
+         */
+        void onResponse(T response);
+    }
+
+    /**
+     * Listener interface for receiving errors.
+     */
+    public interface ErrorListener {
+        /**
+         * Called when an error is received.
+         */
+        void onErrorResponse(VoltraError error);
+    }
+}
